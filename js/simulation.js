6b77ad4777b5660d769c8d2ec9d8bfbb6e5ba7d2
Pham Van Vung <phamvanvung@gmail.com>
2019-11-25 19:51:11 -0600

    Simulate on the data which is closer to the actual data.

--- js/simulation.js
+++ js/simulation.js
@@ -1,13 +1,20 @@
-const N_TRIALS = 2000;
-const N_ARMS = 16;
-const N_FEATURES = 5;
-const BEST_ARMS = [1, 3, 5];
+const N_TRIALS = 200;
+const N_ARMS = 36;
+const N_FEATURES = 17;
+const BEST_ARMS = [1, 3, 5, 9];
+const N_ARMS_TO_RECOMMEND = 4;
+
+// const N_TRIALS = 2000;
+// const N_ARMS = 36;
+// const N_FEATURES = 5;
+// const BEST_ARMS = [1, 3, 5];
+// const N_ARMS_TO_RECOMMEND = 4;
 
 let X = generateData(N_TRIALS, N_ARMS, N_FEATURES);
 let trueTheta = generateTheta(N_ARMS, N_FEATURES, BEST_ARMS, 1.0);
 
 //Add argmax method
-math.argmax = (a)=>a.indexOf(math.max(a));
+math.argmax = (a) => a.indexOf(math.max(a));
 
 /**
  * Simulate the learning process and observe the payoffs
@@ -17,7 +24,7 @@ math.argmax = (a)=>a.indexOf(math.max(a));
  * @param trueTheta {Array: nArms x nFeatures}: true theta used to generate reward
  * @return {Array}: the payoffs for every trial.
  */
-function linUCB(alpha, X, generateReward, trueTheta) {
+function linUCB(alpha, X, generateReward, trueTheta, armsToRecommend) {
     const nTrials = X.length;
     const nArms = X[0].length;
     const nFeatures = X[0][0].length;
@@ -27,12 +34,17 @@ function linUCB(alpha, X, generateReward, trueTheta) {
 
     for (let t = 0; t < nTrials; t++) {
         // compute the estimates (theta) and prediction for all arms
-        let recommendedActions = lucb.recommend(X[t], 1);//Recommend one by now
-        let chosenArm = recommendedActions[0];//Take only one
-        let xChosenArm = X[t][chosenArm];
-        payoffs[t] = generateReward(chosenArm, xChosenArm, trueTheta);
+        let recommendedActions = lucb.recommend(X[t], armsToRecommend);
+        let rewards = [];
+        for (let i = 0; i < recommendedActions.length; i++) {
+            let chosenArm = recommendedActions[i];//Take one arm
+            let xChosenArm = X[t][chosenArm];
+            rewards.push(generateReward(chosenArm, xChosenArm, trueTheta));
+        }
+
+        payoffs[t] = math.sum(rewards);
         // update intermediate object
-        lucb.include(X[t], [chosenArm], [payoffs[t]]);
+        lucb.include(X[t], recommendedActions, rewards);
     }
     return payoffs;
 }
