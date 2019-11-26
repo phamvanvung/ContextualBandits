04e6fe971dbecc25cd174aa8326cef93c8a7b5a5
Pham Van Vung <phamvanvung@gmail.com>
2019-11-26 11:54:34 -0600

    Simulate on the data which is closer to the actual data.

--- js/generatedata.js
+++ js/generatedata.js
@@ -76,3 +76,35 @@ function makeRegret(payoffs, oracles) {
     });
     return cusum;
 }
+/**
+ * Simulate the learning process and observe the payoffs
+ * @param alpha number: this is the :math:`\alpha = \sqrt{ln(2/\sigma)/2}`
+ * @param X {Array: nTrials x nArms x nFeatures}: The observed data
+ * @param generateReward {function}: A function used to generate the reward data
+ * @param trueTheta {Array: nArms x nFeatures}: true theta used to generate reward
+ * @return {Array}: the payoffs for every trial.
+ */
+function linUCB(alpha, X, generateReward, trueTheta, armsToRecommend) {
+    const nTrials = X.length;
+    const nArms = X[0].length;
+    const nFeatures = X[0][0].length;
+    //Initialize A
+    let payoffs = [];
+    let lucb = new LinUCB(alpha, nTrials, nArms, nFeatures);
+
+    for (let t = 0; t < nTrials; t++) {
+        // compute the estimates (theta) and prediction for all arms
+        let recommendedActions = lucb.recommend(X[t], armsToRecommend);
+        let rewards = [];
+        for (let i = 0; i < recommendedActions.length; i++) {
+            let chosenArm = recommendedActions[i];//Take one arm
+            let xChosenArm = X[t][chosenArm];
+            rewards.push(generateReward(chosenArm, xChosenArm, trueTheta));
+        }
+
+        payoffs[t] = math.sum(rewards);
+        // update intermediate object
+        lucb.include(X[t], recommendedActions, rewards);
+    }
+    return payoffs;
+}
--- js/simulation.js
+++ js/simulation.js
@@ -10,41 +10,35 @@ const N_ARMS_TO_RECOMMEND = 4;
 // const BEST_ARMS = [1, 3, 5];
 // const N_ARMS_TO_RECOMMEND = 4;
 
+
+let trials = Array.from(new Array(N_TRIALS), (_, i) => i);
 let X = generateData(N_TRIALS, N_ARMS, N_FEATURES);
 let trueTheta = generateTheta(N_ARMS, N_FEATURES, BEST_ARMS, 1.0);
 
-//Add argmax method
-math.argmax = (a) => a.indexOf(math.max(a));
-
-/**
- * Simulate the learning process and observe the payoffs
- * @param alpha number: this is the :math:`\alpha = \sqrt{ln(2/\sigma)/2}`
- * @param X {Array: nTrials x nArms x nFeatures}: The observed data
- * @param generateReward {function}: A function used to generate the reward data
- * @param trueTheta {Array: nArms x nFeatures}: true theta used to generate reward
- * @return {Array}: the payoffs for every trial.
- */
-function linUCB(alpha, X, generateReward, trueTheta, armsToRecommend) {
-    const nTrials = X.length;
-    const nArms = X[0].length;
-    const nFeatures = X[0][0].length;
-    //Initialize A
-    let payoffs = [];
-    let lucb = new LinUCB(alpha, nTrials, nArms, nFeatures);
-
-    for (let t = 0; t < nTrials; t++) {
-        // compute the estimates (theta) and prediction for all arms
-        let recommendedActions = lucb.recommend(X[t], armsToRecommend);
-        let rewards = [];
-        for (let i = 0; i < recommendedActions.length; i++) {
-            let chosenArm = recommendedActions[i];//Take one arm
-            let xChosenArm = X[t][chosenArm];
-            rewards.push(generateReward(chosenArm, xChosenArm, trueTheta));
-        }
-
-        payoffs[t] = math.sum(rewards);
-        // update intermediate object
-        lucb.include(X[t], recommendedActions, rewards);
+let oracles = X.map(trial => {
+    //Generate array of scores => then sort ascending => then take the last N_ARMS (largest)
+    return math.sum((trial.map((arm, i) => {
+        return math.dot(arm, trueTheta[i]);
+    })).sort((a, b) => a - b).slice(-N_ARMS_TO_RECOMMEND));
+});
+let payoffsRandom = X.map(trial => {
+    let totalRewards = 0;
+    for (let i = 0; i < N_ARMS_TO_RECOMMEND; i++) {
+        //Select one random arm
+        let randomArm = d3.randomInt(0, N_ARMS)();
+        //Add the score
+        totalRewards += math.dot(trial[randomArm], trueTheta[randomArm]);
     }
-    return payoffs;
-}
+    return totalRewards;
+});
+
+let regretsRandom = makeRegret(payoffsRandom, oracles);
+let regrets = {};
+regrets['random'] = regretsRandom;
+
+let alphas = [0, 1.0, 2.5, 5.0, 10.0];
+alphas.forEach(alpha => {
+    let payoffsAlpha = linUCB(alpha, X, generateReward, trueTheta, N_ARMS_TO_RECOMMEND);
+    regrets['alpha=' + alpha] = makeRegret(payoffsAlpha, oracles);
+});
+
